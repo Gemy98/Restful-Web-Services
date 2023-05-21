d801c88beade008e0972613e78e8fdaafa72387f
Gemy98 <mohamedgamal19981998@gmail.com>
2023-05-21 05:37:08 +0300

    get value from URL (@PathVariable) and review on @postconstruct and pre destroy

--- src/main/java/com/spring/restful/controller/MainController.java
+++ src/main/java/com/spring/restful/controller/MainController.java
@@ -3,8 +3,12 @@ package com.spring.restful.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +21,25 @@ import com.spring.restful.model.Student;
 //localhost 9090 /restful/main
 public class MainController {
 
+	
+	private List<Student> students = new ArrayList<>();
+	
+	@PostConstruct
+	public void start() {
+		Student s1 = new Student(1,"Ahmed","01025446634");
+		Student s2 = new Student(2,"Mohamed","0100000000");
+		Student s3 = new Student(3,"Gemy","01025446484");
+		Student s4 = new Student(4,"Shaaban","01025416634");
+		Student s5 = new Student(5,"Mostafa","01025464634");
+	
+		students.add(s1);
+		students.add(s2);
+		students.add(s3);
+		students.add(s4);
+		students.add(s5);
+		
+	}
+
 	//localhost 9090 /restful/main/facebook
 	@GetMapping("/facebook")
 	public String gerMain() {
@@ -27,24 +50,27 @@ public class MainController {
 	@GetMapping("/students")
 	public List<Student> getStudents(){
 	
-		Student s1 = new Student(1,"Ahmed","01025446634");
-		Student s2 = new Student(2,"Mohamed","0100000000");
-		Student s3 = new Student(3,"Gemy","01025446484");
-		Student s4 = new Student(4,"Shaaban","01025416634");
-		Student s5 = new Student(5,"Mostafa","01025464634");
 	
-		List<Student> students = new ArrayList<>();
-		students.add(s1);
-		students.add(s2);
-		students.add(s3);
-		students.add(s4);
-		students.add(s5);
 		
 		return students ; 
-		
-		
-		
-		
 	}  
+	
+	//localhost8080/restful/main/getStudent/id
+	@GetMapping("/getStudent/{id}")
+	public Student getStudentById(@PathVariable("id") int id) {
+		
+		
+		
+		return students.get(id - 1);
+		
+		
+	}
+	
+	@PreDestroy
+	public void end() {
+		students.clear();
+	}
+	
+	
 	
 }
