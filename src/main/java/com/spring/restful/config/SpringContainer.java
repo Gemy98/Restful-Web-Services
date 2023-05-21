c56366e86d23f0a217aa909f1735a71488cae7f0
Gemy98 <mohamedgamal19981998@gmail.com>
2023-05-21 04:39:47 +0300

    create SpringContainer and webXML class

---
+++ src/main/java/com/spring/restful/config/SpringContainer.java
@@ -0,0 +1,14 @@
+package com.spring.restful.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan("com.spring")
+@EnableWebMvc
+public class SpringContainer {
+
+	
+	
+}
